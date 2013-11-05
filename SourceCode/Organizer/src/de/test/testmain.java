561a8ec367fcadc7e9357ab9ba41f33e97fdd798
Jens <game90@hotmail.de>
2013-11-05 17:30:51 +0700

    abcde

--- SourceCode/Organizer/src/de/test/testmain.java
+++ SourceCode/Organizer/src/de/test/testmain.java
@@ -8,7 +8,7 @@ public class testmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
-		System.out.println("test1");
+		System.out.println("test2");
 
 	}
 
